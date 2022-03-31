1edbf8d9418b19d6570f0704a9fdf15ffa8c91f6
Joe Frostick <joe.frostick@tesco.com>
2022-03-31 12:47:22 +0100

    Makes InMemoryPantryRepository a singleton

--- app/src/main/java/uk/co/bubblebearapps/pantry/di/DataModule.kt
+++ app/src/main/java/uk/co/bubblebearapps/pantry/di/DataModule.kt
@@ -6,11 +6,13 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import uk.co.bubblebearapps.pantry.data.InMemoryPantryRepository
 import uk.co.bubblebearapps.pantry.data.PantryRepository
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal interface DataModule {
 
     @Binds
+    @Singleton
     fun pantryRepo(impl: InMemoryPantryRepository): PantryRepository
 }
