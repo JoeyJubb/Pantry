95a6d68ba3a2064c900fbcdca049fbab78d15ac2
Joe Frostick <joe.frostick@tesco.com>
2022-04-01 10:01:29 +0100

    Adds UseCaseTest.kt

---
+++ core/src/test/java/uk/co/bubblebearapps/pantry/domain/UseCaseTest.kt
@@ -0,0 +1,40 @@
+package uk.co.bubblebearapps.pantry.domain
+
+import arrow.core.Either
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.amshove.kluent.shouldBeEqualTo
+
+import org.junit.Test
+import java.lang.Exception
+
+@ExperimentalCoroutinesApi
+class UseCaseTest {
+
+    @Test
+    fun `a working Testcase results in a Right`() = runTest {
+        val response = "and you know it"
+
+        val worksFine = object : UseCase<String, String>() {
+            override suspend fun doWork(params: String) : String{
+                return response
+            }
+        }
+
+        worksFine("if you're happy") shouldBeEqualTo Either.Right(response)
+    }
+
+    @Test
+    fun `a broken Testcase results in a Left`() = runTest {
+        val exception = mockk<Exception>()
+
+        val broken = object : UseCase<String, String>() {
+            override suspend fun doWork(params: String) : String{
+                throw exception
+            }
+        }
+
+        broken("clap your hands") shouldBeEqualTo Either.Left(exception)
+    }
+}
